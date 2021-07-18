05207038964f586e5893faff527ad8cb09892f3c
borkogrujicic <borko.grujicic1@gmail.com>
2021-07-18 20:49:27 +0200

    Search by ticked price

--- cinema-front/src/components/Projections/Projections.js
+++ cinema-front/src/components/Projections/Projections.js
@@ -8,6 +8,8 @@ const Projections = () => {
   const [pageNo, setPageNo] = useState("");
   const [totalPages, setTotalPages] = useState("");
   const [showSearchForm, setShowSearchForm] = useState(false);
+  const [tip, setTip] = useState("");
+  const [cenaKarteOd, setCenaKarteOd] = useState('');
 
   useEffect(() => {
     getProjections(0);
@@ -37,6 +39,14 @@ const Projections = () => {
     getProjections(page);
   };
 
+  const tipProjekcijeChange = (event) => {
+    setTip(event.target.value);
+  };
+
+  const cenaKarteOdChange = (event) => {
+    setCenaKarteOd(event.target.value);
+  };
+
   return (
     <div>
       <Form.Group style={{ marginTop: 35 }}>
@@ -47,29 +57,37 @@ const Projections = () => {
         />
       </Form.Group>
       <Collapse in={showSearchForm}>
-              <Form style={{ marginTop: 10 }}>
+        <Form style={{ marginTop: 10 }}>
           <Form.Group>
             <Form.Label>Naziv filma</Form.Label>
-            <Form.Control
-              name="naziv"
-              as="input"
-            ></Form.Control>
+            <Form.Control name="naziv" as="input"></Form.Control>
           </Form.Group>
           <Form.Label htmlFor="pTip">Tip projekcije</Form.Label>
-              <Form.Control
-                name="tip"
-                as="select"
-                id="tip"
-                // onChange={(e) => ValueInputChange(e)}
-              >
-                <option></option>
-                <option value="2D">2D</option>
-                <option value="3D">3D</option>
-                <option value="4D">4D</option>
-              </Form.Control>
-          </Form>
-
-      </Collapse> 
+          <Form.Control
+            name="tip"
+            as="select"
+            value={tip}
+            id="tip"
+            onChange={(e) => tipProjekcijeChange(e)}
+          >
+            <option></option>
+            <option value="2D">2D</option>
+            <option value="3D">3D</option>
+            <option value="4D">4D</option>
+          </Form.Control>
+          <Form.Group>
+            <Form.Label>Cena karte od:</Form.Label>
+            <Form.Control
+              onChange={(event) => cenaKarteOdChange(event)}
+              name="cenaKarteOd"
+              value={cenaKarteOd}
+              as="input"
+              type="number"
+              step="0.1"
+            ></Form.Control>
+          </Form.Group>
+        </Form>
+      </Collapse>
       <ProjectionList projections={projekcije} />
       <Button disabled={pageNo == 0} onClick={() => changePage(-1)}>
         Previous
